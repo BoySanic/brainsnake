e0a3cfb3898610af840395f2e8e1eb15aa81245d
boysanic <boysonicrevived@gmail.com>
2019-08-08 00:40:16 -0400

    Improved

--- brainsnake.py
+++ brainsnake.py
@@ -1,26 +1,23 @@
 import sys
-
-
+import numpy as np
 
 def interpret(brainfuck):	
 	programcounter = 0
 	pointer = 0
 	cells = [0] * 32768
-	inLoop = False
-	ignoreNext = False
 	char = ""
 	loopdict = {
 
 	}
+	dictloop = {
+
+	}
 	while(True):
 		char = brainfuck[programcounter]
-		#print(char)
 		if(char == '>'):
-				pointer += 1
-				#print(pointer)
+			pointer += 1
 		if(char == '<'):
 			pointer -= 1
-			#print(pointer)
 		if(char == '+'):
 			if(cells[pointer] == 255):
 				cells[pointer] = 0
@@ -38,50 +35,30 @@ def interpret(brainfuck):
 		if(char == ','):
 			cells[pointer] = raw_input("What do you want to enter: ")
 		if(char == '['):
-			#find matching closing bracket
 			c = char
 			p = programcounter
 			i = 0
-			while(True):
-				c = brainfuck[p]
-				if(c == '['):
-					i += 1
-				if(c == ']'):
-					i -= 1
-				if(i == 0):
-		#			print("The closing bracket is at %s" % p)
-					loopdict[p] = programcounter
-		#			print(loopdict)	
-					break
-				p += 1
-			#append to dict
-			if(cells[pointer] == 0):
-				#print("Woah we're leaving a loop")
-				i = 0
+			if(dictloop.has_key(programcounter) != True):
 				while(True):
-					c = brainfuck[programcounter]
+					c = brainfuck[p]
 					if(c == '['):
 						i += 1
 					if(c == ']'):
 						i -= 1
 					if(i == 0):
+						loopdict[p] = programcounter
+						dictloop[programcounter] = p
 						break
-					programcounter += 1
+					p += 1
+			if(cells[pointer] == 0):
+				programcounter = dictloop[programcounter]
 		if(char == ']'):
-			#print("We going back?")
-			if(ignoreNext != True):
 				if(cells[pointer] != 0):
-					#print("Yes" + str(programcounter))
 					programcounter = loopdict[programcounter]
-					#print(char + str(programcounter))
-				#else:
-					#print("Nah" + str(programcounter))
 		if(programcounter == len(brainfuck)):
 			print("fucking")
 			break
-		#print("DEBUG: programcounter=%i pointer=%i cells[pointer] = %i, char=%s" % (programcounter, pointer, cells[pointer], char))
 		programcounter += 1
-		#raw_input()
 
 
 def main():
